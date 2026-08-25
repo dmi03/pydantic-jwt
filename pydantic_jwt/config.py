05837f6e46de59b98365937b8a9803dbf74f0106
dmi03 <git@dmi03.com>
2026-08-25 20:12:04 +0300

    feat: add ConfigDict extension with algorithm/key/require_keys options

---
+++ pydantic_jwt/config.py
@@ -0,0 +1,8 @@
+from pydantic import ConfigDict as PydanticConfigDict
+
+
+class ConfigDict(PydanticConfigDict, total=False):
+    algorithm: str
+    encoding_key: str | None
+    decoding_key: str | None
+    require_keys: bool
