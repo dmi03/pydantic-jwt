6a94c70b7342b81ed3f6834a6c46ba17581ed738
dmi03 <git@dmi03.com>
2026-08-28 21:09:03 +0300

    feat: export AudClaim, IssClaim and uuid

--- pydantic_jwt/__init__.py
+++ pydantic_jwt/__init__.py
@@ -1,19 +1,22 @@
 from .base import JWTModel
-from .claims import Claim, Exp, ExpClaim, Iat, IatClaim, Nbf, NbfClaim, after, at
+from .claims import AudClaim, Claim, Exp, ExpClaim, Iat, IatClaim, IssClaim, Nbf, NbfClaim, after, at, uuid
 from .config import ConfigDict
 from .str import JWTStr
 
 __all__ = [
+    "AudClaim",
     "Claim",
     "ConfigDict",
     "Exp",
     "ExpClaim",
     "Iat",
     "IatClaim",
+    "IssClaim",
     "JWTModel",
     "JWTStr",
     "Nbf",
     "NbfClaim",
     "after",
     "at",
+    "uuid",
 ]
